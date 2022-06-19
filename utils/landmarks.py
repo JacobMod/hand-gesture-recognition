ab045c68f6da3da10ddb8ac902bbec304859aaab
Jakub Modrzejewski <jakub.modrzejewski@forcom.com.pl>
2022-06-19 14:46:53 +0200

    List landmarks as flat array

--- utils/landmarks.py
+++ utils/landmarks.py
@@ -5,18 +5,17 @@ from typing import List
 from pandas import DataFrame
 
 
-def list_landmarks(landmarks) -> List:
+def list_landmarks(landmarks) -> List[float]:
     landmark_points = []
 
     for landmark in landmarks.landmark:
-        landmark_points.append([landmark.x, landmark.y])
+        landmark_points.extend([landmark.x, landmark.y])
 
     return landmark_points
 
 
-def add_landmarks_as_row(class_id: int, landmarks_list: List, df: DataFrame) -> None:
+def add_landmarks_as_row(class_id: int, landmarks_list: List[float], df: DataFrame) -> None:
     list_to_insert = deepcopy(landmarks_list)
-    list_to_insert = list(chain(*list_to_insert))
     list_to_insert.insert(0, class_id)
     df_length = len(df)
     df.loc[df_length] = list_to_insert
